1b81a7674c69868b91fbfd00fedd0dfe2afa7f6d
JAPJEET SINGH <japjeet612003@gmail.com>
2023-07-06 22:32:12 +0530

    c++

---
+++ fabinoi series/fabinoi_series.cpp
@@ -0,0 +1,27 @@
+#include  <iostream>
+using namespace std ; 
+
+void fbi( int n){
+    int tn = 0 ;
+    int ts = 1 ;
+    int tp ; 
+    for (int i = 1; i <= n; i++)
+    {
+        
+        cout  << tn <<","; 
+        tp = tn + ts ; 
+        tn =  ts ;
+        ts =  tp  ;
+
+
+
+    }
+    
+}
+
+int main(){
+    int n ;
+    cin >>n ;
+    fbi(n) ; 
+
+}
