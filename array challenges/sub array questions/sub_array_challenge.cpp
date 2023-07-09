6d7f03f8ee1133dbafd9d22257b33e95e5e01b28
JAPJEET SINGH <japjeet612003@gmail.com>
2023-07-09 12:24:54 +0530

    m

---
+++ array challenges/sub array questions/sub_array_challenge.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int  n ;
+    cin>>n  ;
+    int arr[n] ;
+    int sum  = 0  ;
+    int indexi  = 0  ;
+    int indexj  = 0  ;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i] ;
+
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j <n ; j++)
+        {
+            sum += (arr[i] + arr[j]) ;
+            // cout<<sum <<endl; 
+            index 
+        }
+            cout<<"SUM OF INDEX FROM "<< i <<" TO " << n <<" IS : " << sum  <<endl ;
+        
+    }
+    
+
+
+    
+    
+
+}
