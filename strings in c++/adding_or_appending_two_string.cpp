d05f4057fee8cb746444531e00875daa4c8b6e24
JAPJEET SINGH <95476734+JAPJEET01@users.noreply.github.com>
2023-07-16 10:45:04 +0530

    .

---
+++ strings in c++/adding_or_appending_two_string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ; 
+int  main (){
+    string str1 =  "fam" ; 
+    string str2 =  "ily" ; 
+    string str3 =  str1 + str2 ; 
+
+    cout <<str1 <<endl;
+
+
+                                        // or
+
+    
+    str1.append(str2) ;
+    cout <<str1 <<endl;
+
+
+}
